c76b46579491f0a43d73376299665438868669fe
Steetan <vlad.no.96@bk.ru>
2023-01-26 18:48:33 +0300

    optimize proj

--- js/main.js
+++ js/main.js
@@ -19,54 +19,60 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-for (const i of galleryBtn) {
-    i.addEventListener("click", function(event) {
-        i.classList.toggle("active")
+galleryBtn.forEach(function(e) {
+    e.addEventListener("click", function(event) {
+        e.classList.toggle("active")
 
-        if(i.classList.contains("active")) {
-            i.innerHTML = "скрыть"
-        } else {
-            i.innerHTML = "показать еще"
-        }
-        if(i.classList.contains("gallery-1")) {
+        e.classList.contains("active")
+        ? (e.innerHTML = "скрыть")
+        : (e.innerHTML = "показать еще");
+
+        if(e.classList.contains("gallery-1")) {
             document.querySelector(".gallery__block2.gallery-1").classList.toggle("active")
         }
-        if(i.classList.contains("gallery-2")) {
+        if(e.classList.contains("gallery-2")) {
             document.querySelector(".gallery__block2.gallery-2").classList.toggle("active")
         }
-        if(i.classList.contains("gallery-3")) {
+        if(e.classList.contains("gallery-3")) {
             document.querySelector(".gallery__block2.gallery-3").classList.toggle("active")
         }
-        if(i.classList.contains("gallery-4")) {
+        if(e.classList.contains("gallery-4")) {
             document.querySelector(".gallery__block2.gallery-4").classList.toggle("active")
         }
     })
-}
+})
 
 galleryLink.forEach(function(choiceItem) {
     choiceItem.addEventListener('click', function(event) {
-        galleryBlock.forEach(function() {
-            i.classList.remove("active")
+        galleryBlock.forEach(function(e) {
+            e.classList.remove("active")
         })
-        galleryLink.forEach(function(i) {
-            i.classList.remove("active")
+        galleryLink.forEach(function(e) {
+            e.classList.remove("active")
         })
         choiceItem.classList.toggle("active");
 
-        galleryBlocks.forEach(function(i) {
-            i.classList.remove("active")
+        galleryBlocks.forEach(function(e) {
+            e.classList.remove("active")
         })
-        if(choiceItem.classList.contains("gallery-1")) {
-            document.querySelector(".gallery__blocks.gallery-1").classList.add("active")
-        }
-        if(choiceItem.classList.contains("gallery-2")) {
-            document.querySelector(".gallery__blocks.gallery-2").classList.add("active")
-        }
-        if(choiceItem.classList.contains("gallery-3")) {
-            document.querySelector(".gallery__blocks.gallery-3").classList.add("active")
-        }
-        if(choiceItem.classList.contains("gallery-4")) {
-            document.querySelector(".gallery__blocks.gallery-4").classList.add("active")
+
+        switch (choiceItem.className) {
+            case "gallery__link gallery-1 active":
+                document.querySelector(".gallery__blocks.gallery-1")
+                    .classList.add("active")
+                break;
+            case "gallery__link gallery-2 active":
+                document.querySelector(".gallery__blocks.gallery-2")
+                    .classList.add("active")
+                break;
+            case "gallery__link gallery-3 active":
+                document.querySelector(".gallery__blocks.gallery-3")
+                    .classList.add("active")
+                break;
+            case "gallery__link gallery-4 active":
+                document.querySelector(".gallery__blocks.gallery-4")
+                    .classList.add("active")
+                break;
         }
     });
 });
@@ -85,51 +91,55 @@ menuBurger.addEventListener("click", function(e) {
     : (document.body.style.overflow = "auto");
 })
 
-for (const link of menuLink) {
-    link.addEventListener("click", function(e) {
+menuLink.forEach(function(event) {
+    event.addEventListener("click", function(e) {
         e.preventDefault();
-        if(link.classList.contains("menu__link--surfer")) {
-            document.querySelector(".surfer")
-                .scrollIntoView({
-                    block: "start",
-                    behavior: "smooth",
-                })
-        }
-        if(link.classList.contains("menu__link--services")) {
-            document.querySelector(".services")
-                .scrollIntoView({
-                    block: "start",
-                    behavior: "smooth",
-                })
-        }
-        if(link.classList.contains("menu__link--blog")) {
-            document.querySelector(".blog")
-                .scrollIntoView({
-                    block: "start",
-                    behavior: "smooth",
-                })
-        }
-        if(link.classList.contains("menu__link--team")) {
-            document.querySelector(".team")
-                .scrollIntoView({
-                    block: "start",
-                    behavior: "smooth",
-                })
-        }
-        if(link.classList.contains("menu__link--contacts")) {
-            document.querySelector(".contacts")
-                .scrollIntoView({
-                    block: "start",
-                    behavior: "smooth",
-                })
-        }
-        document.body.style.overflow = "auto";
-        document.querySelector(".menu--active")
-            .classList.remove("menu--active")
-        document.querySelector(".menu__list--active")
-            .classList.remove("menu__list--active")
-        document.querySelector(".menu-burger--open")
-            .classList.remove("menu-burger--open")
-    })
-}
 
+        switch (event.className) {
+            case "menu__link menu__link--surfer":
+                document.querySelector(".surfer")
+                .scrollIntoView({
+                    block: "start",
+                    behavior: "smooth",
+                })
+                break;
+            case "menu__link menu__link--services":
+                document.querySelector(".services")
+                .scrollIntoView({
+                    block: "start",
+                    behavior: "smooth",
+                })
+                break;
+            case "menu__link menu__link--blog":
+                document.querySelector(".blog")
+                .scrollIntoView({
+                    block: "start",
+                    behavior: "smooth",
+                })
+                break;
+            case "menu__link menu__link--team":
+                document.querySelector(".team")
+                .scrollIntoView({
+                    block: "start",
+                    behavior: "smooth",
+                })
+                break;
+            case "menu__link menu__link--contacts":
+                document.querySelector(".contacts")
+                .scrollIntoView({
+                    block: "start",
+                    behavior: "smooth",
+                })
+                break;
+        
+        }
+
+        document.body.style.overflow = "auto";
+        document.querySelector(".menu")
+            .classList.remove("menu--active")
+        document.querySelector(".menu__list")
+            .classList.remove("menu__list--active")
+        document.querySelector(".menu-burger")
+            .classList.remove("menu-burger--open")
+     })
+})
